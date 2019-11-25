fb81dd9006020ad113c5bfb6ceea3ca1a5b21a75
sarahzientek2 <sarahezientek@gmail.com>
2019-11-25 15:18:58 +0000

    updates

--- test/products.test.js
+++ test/products.test.js
@@ -19,3 +19,19 @@ describe('Getting products', () => {
         expect(res.body).to.have.property('data').to.deep.equal({id: 1, name: "Laptop"}) 
     })
 })
+
+describe('Creating products', () => {
+    it('should create correctly', async () => {
+        let newProduct = {name: "Printer"}
+        varres = await request(app)
+        .post('/api/products')
+        .send(newProduct)
+        expect(res.statusCode).equals(201)
+        expect(res.body).to.have.property('data').to.have.property('message','Created ok')
+        expect(res.body).to.have.property('data').to.have.property('id')
+        const id = res.body.data.id
+        res = await request(app)
+        .get(`/api/products/${id}`)expect(res.statusCode).equals(200)
+        expect(res.body).to.have.nested.property('data.name','Printer')
+        })
+})
